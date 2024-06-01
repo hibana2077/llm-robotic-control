cd9059669577bb973fe099c946f0ee20a1c44487
hibana2077 <hibana2077@gmail.com>
2024-06-01 17:02:23 +0800

    chore: Add audio recording functionality to Chat page

--- src/st_web/pages/Chat.py
+++ src/st_web/pages/Chat.py
@@ -3,6 +3,7 @@ import requests
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from langchain_core.output_parsers import JsonOutputParser
+from audiorecorder import audiorecorder
 import os
 import yaml
 """
@@ -23,4 +24,15 @@ Workflow:
 
 st.title("Voice Robot Control")
 
+st.title("Audio Recorder")
+audio = audiorecorder("Click to record", "Click to stop recording")
 
+if len(audio) > 0:
+    # To play audio in frontend:
+    st.audio(audio.export().read())  
+
+    # To save audio to a file, use pydub export method:
+    audio.export("audio.wav", format="wav")
+
+    # To get audio properties, use pydub AudioSegment properties:
+    st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
