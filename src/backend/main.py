406623eb39b63e5cfcb581cf4be6bb635390dda6
hibana2077 <hibana2077@gmail.com>
2024-06-02 23:10:30 +0800

    chore: Add endpoint for removing tasks from the task queue

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-05-17 21:46:09
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-06-02 22:45:59
+LastEditTime: 2024-06-02 23:10:05
 FilePath: \llm-robotic-control\src\backend\main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -129,6 +129,13 @@ async def get_tasks():
     # get all tasks from redis
     tasks = redis_client.lrange("task_queue", 0, -1)
     return tasks
+
+@app.post("/remove_task")
+async def remove_task(data: dict):
+    task:str = data["task"]
+    # remove task from redis
+    redis_client.lrem("task_queue", 0, task)
+    return {"status": "success"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host=HOST, port=8000)
