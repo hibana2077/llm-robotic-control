7c3e4c498cc6e45618876d3c1bf58bba8095c1af
hibana2077 <hibana2077@gmail.com>
2024-06-04 10:09:39 +0800

    chore: Update Task.py with recent changes and improve code readability

--- src/st_web/pages/Task.py
+++ src/st_web/pages/Task.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-06-02 21:35:59
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-06-02 23:49:48
+LastEditTime: 2024-06-04 10:09:34
 FilePath: \llm-robotic-control\src\st_web\pages\Task.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -54,6 +54,8 @@ if st.button("Execute Task"):
 task = json.loads(selected_task)[0]
 
 # According to the action in the task, send control signals to database and robotic arm will get the signals from database
+
+
 
 # Wait for the robotic arm to complete the action (loop until the action is completed(sleep for 1 second))
 
