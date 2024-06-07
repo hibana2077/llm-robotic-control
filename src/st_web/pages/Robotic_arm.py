e32574a2bfec198efd7dfbfb0fd9f459cf550f1d
hibana2077 <hibana2077@gmail.com>
2024-06-07 13:53:53 +0800

    chore: Update Chat.py and Task.py with multilingual chat functionality and robotic arm control logic

---
+++ src/st_web/pages/Robotic_arm.py
@@ -0,0 +1,9 @@
+'''
+Author: hibana2077 hibana2077@gmail.com
+Date: 2024-06-07 13:13:30
+LastEditors: hibana2077 hibana2077@gmail.com
+LastEditTime: 2024-06-07 13:13:57
+FilePath: \llm-robotic-control\src\st_web\pages\Robotic_arm.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import streamlit as st
--- src/st_web/pages/Task.py
+++ src/st_web/pages/Task.py
@@ -2,12 +2,11 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-06-02 21:35:59
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-06-06 23:07:47
+LastEditTime: 2024-06-07 13:11:02
 FilePath: \llm-robotic-control\src\st_web\pages\Task.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 import streamlit as st
-import translators as ts
 import requests
 import json
 import os
